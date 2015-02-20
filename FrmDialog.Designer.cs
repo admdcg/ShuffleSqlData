3fbbe93ead683cc0bfebba45ed7a90b38980516c
admdcg <admdcg@gmail.com>
2015-02-20 19:22:21 +0000

    First upload

---
+++ FrmDialog.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace ShuffleApplication
+{
+    partial class FrmDialog
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Tablas");
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CmdOpenDialog = new System.Windows.Forms.Button();
+            this.TxtConnectionString = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.TvwTables = new System.Windows.Forms.TreeView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CmdPrevious = new System.Windows.Forms.Button();
+            this.CmdExit = new System.Windows.Forms.Button();
+            this.CmdNext = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(876, 430);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.CmdOpenDialog);
+            this.tabPage1.Controls.Add(this.TxtConnectionString);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(868, 404);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Connection string";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cadena de Conexión";
+            // 
+            // CmdOpenDialog
+            // 
+            this.CmdOpenDialog.Location = new System.Drawing.Point(169, 81);
+            this.CmdOpenDialog.Name = "CmdOpenDialog";
+            this.CmdOpenDialog.Size = new System.Drawing.Size(26, 23);
+            this.CmdOpenDialog.TabIndex = 1;
+            this.CmdOpenDialog.Text = "...";
+            this.CmdOpenDialog.UseVisualStyleBackColor = true;
+            this.CmdOpenDialog.Click += new System.EventHandler(this.CmdOpenDialog_Click);
+            // 
+            // TxtConnectionString
+            // 
+            this.TxtConnectionString.Location = new System.Drawing.Point(201, 84);
+            this.TxtConnectionString.Multiline = true;
+            this.TxtConnectionString.Name = "TxtConnectionString";
+            this.TxtConnectionString.Size = new System.Drawing.Size(614, 113);
+            this.TxtConnectionString.TabIndex = 0;
+            this.TxtConnectionString.Text = "Data Source=SRVSQL;Initial Catalog=ChequesXPIntraSoftTest;Persist Security Info=T" +
+    "rue;User ID=sa;Password=IntrasoftSA2027";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.TvwTables);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(868, 404);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Tablas";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // TvwTables
+            // 
+            this.TvwTables.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TvwTables.Location = new System.Drawing.Point(3, 3);
+            this.TvwTables.Name = "TvwTables";
+            treeNode1.Name = "Tables";
+            treeNode1.Text = "Tablas";
+            this.TvwTables.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.TvwTables.Size = new System.Drawing.Size(862, 398);
+            this.TvwTables.TabIndex = 0;
+            this.TvwTables.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.TvwTables_AfterCheck);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.CmdPrevious);
+            this.panel1.Controls.Add(this.CmdExit);
+            this.panel1.Controls.Add(this.CmdNext);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 430);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(876, 71);
+            this.panel1.TabIndex = 1;
+            // 
+            // CmdPrevious
+            // 
+            this.CmdPrevious.Location = new System.Drawing.Point(582, 24);
+            this.CmdPrevious.Name = "CmdPrevious";
+            this.CmdPrevious.Size = new System.Drawing.Size(75, 23);
+            this.CmdPrevious.TabIndex = 8;
+            this.CmdPrevious.Text = "<- Anteri&or";
+            this.CmdPrevious.UseVisualStyleBackColor = true;
+            // 
+            // CmdExit
+            // 
+            this.CmdExit.Location = new System.Drawing.Point(663, 24);
+            this.CmdExit.Name = "CmdExit";
+            this.CmdExit.Size = new System.Drawing.Size(75, 23);
+            this.CmdExit.TabIndex = 7;
+            this.CmdExit.Text = "Salir";
+            this.CmdExit.UseVisualStyleBackColor = true;
+            this.CmdExit.Click += new System.EventHandler(this.CmdExit_Click);
+            // 
+            // CmdNext
+            // 
+            this.CmdNext.Location = new System.Drawing.Point(744, 24);
+            this.CmdNext.Name = "CmdNext";
+            this.CmdNext.Size = new System.Drawing.Size(75, 23);
+            this.CmdNext.TabIndex = 6;
+            this.CmdNext.Text = "Siguie&nte ->";
+            this.CmdNext.UseVisualStyleBackColor = true;
+            this.CmdNext.Click += new System.EventHandler(this.CmdNext_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // FrmDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 503);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "FrmDialog";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button CmdOpenDialog;
+        private System.Windows.Forms.TextBox TxtConnectionString;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button CmdPrevious;
+        private System.Windows.Forms.Button CmdExit;
+        private System.Windows.Forms.Button CmdNext;
+        private System.Windows.Forms.TreeView TvwTables;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ FrmDialog.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Data.ConnectionUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShuffleApplication
+{
+    public partial class FrmDialog : Form
+    {
+        public FrmDialog()
+        {
+            InitializeComponent();
+        }
+        private SqlConnection _connection;
+        private SqlConnection Connection
+        {
+            get
+            {                
+                return _connection;
+            }
+            set
+            {
+                _connection = value;
+            }                
+        }        
+
+        private void CmdOpenDialog_Click(object sender, EventArgs e)
+        {
+            DataConnectionDialog dcd = new DataConnectionDialog();
+            DataSource.AddStandardDataSources(dcd);
+            DataConnectionDialog.Show(dcd);
+            if (dcd.DialogResult == System.Windows.Forms.DialogResult.OK)            
+            {
+                TxtConnectionString.Text = dcd.ConnectionString;                                
+            } 
+
+            else
+            {
+
+            }                                   
+        }       
+        private void CmdNext_Click(object sender, EventArgs e)
+        {            
+            TvwTables.Nodes.Clear();
+            Connection = new SqlConnection(TxtConnectionString.Text);
+            Connection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' ORDER BY table_name", Connection);
+            TvwTables.CheckBoxes = true;
+            SqlConnectionStringBuilder builder=new SqlConnectionStringBuilder(TxtConnectionString.Text);
+            TreeNode nodeDatabase = new TreeNode(builder.InitialCatalog);
+            nodeDatabase.Name = "Database";
+            nodeDatabase.Checked = true;            
+            TreeNode nodeTables = new TreeNode("Tablas");
+            nodeTables.Name = "Tables";
+            nodeTables.Checked = true;
+            nodeDatabase.Nodes.Add(nodeTables);                
+            TvwTables.Nodes.Add(nodeDatabase);
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {                       
+                    TreeNode nodeColumns = new TreeNode("Columnas");
+                    nodeColumns.Name = "Columns";
+                    TreeNode nodeTable = new TreeNode(reader.GetString(0));
+                    nodeTable.Name = "Table";
+                    nodeTable.Nodes.Add(nodeColumns);
+                    nodeTables.Nodes.Add(nodeTable);                        
+                }
+            }            
+            tabControl1.SelectedTab = tabPage2;
+            nodeTables.Expand();
+        }
+
+        private void TvwTables_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            if (e.Action == TreeViewAction.Unknown) return;  //Evita la reentrada              
+            
+            if(e.Node.Name == "Table") // Checkea una Tabla            
+            {                
+                if (e.Node.Checked)
+                {
+                    // Carga columnas
+                    SqlCommand cmd = new SqlCommand("Select column_name from INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @table ORDER BY ORDINAL_POSITION", Connection);
+                    cmd.Parameters.Add("@table", SqlDbType.VarChar);
+                    cmd.Parameters["@table"].Value = e.Node.Text;
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            TreeNode nodeColumn = new TreeNode(reader.GetString(0));
+                            nodeColumn.Name = "Colunm";
+                            TreeNode nodeColumns = e.Node.FirstNode;
+                            nodeColumns.Nodes.Add(nodeColumn);                            
+                        }
+                    }
+                }
+                else
+                {
+                    // Vacío columnas
+                    TreeNode nodeColumns = e.Node.FirstNode;
+                    nodeColumns.Nodes.Clear();
+                }
+            } 
+            else if(e.Node.Name == "Columns") // Checkea en Columnas               
+            {                              
+                foreach (TreeNode nodeCol in e.Node.Nodes)
+                {
+                    nodeCol.Checked = e.Node.Checked;
+                }                               
+            }            
+        }
+
+        private void CmdExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }      
+    }
+}
